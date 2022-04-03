7f7a6a482c1ee3f3e0136347a1aaad7e39200c07
Maryam Naveed <63284481+mareyam@users.noreply.github.com>
2022-04-03 12:19:49 +0500

    Update headline.js

--- headline.js
+++ headline.js
@@ -1,183 +1,123 @@
-jQuery(document).ready(function($){
-	//set animation timing
-	var animationDelay = 2500,
-		//loading bar effect
-		barAnimationDelay = 3800,
-		barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
-		//letters effect
-		lettersDelay = 50,
-		//type effect
-		typeLettersDelay = 150,
-		selectionDuration = 500,
-		typeAnimationDelay = selectionDuration + 800,
-		//clip effect 
-		revealDuration = 600,
-		revealAnimationDelay = 1500;
-	
-	initHeadline();
-	
+(function($){
 
-	function initHeadline() {
-		//insert <i> element for each letter of a changing word
-		singleLetters($('.cd-headline.letters').find('b'));
-		//initialise headline animation
-		animateHeadline($('.cd-headline'));
-	}
+	"use strict";
 
-	function singleLetters($words) {
-		$words.each(function(){
-			var word = $(this),
-				letters = word.text().split(''),
-				selected = word.hasClass('is-visible');
-			for (i in letters) {
-				if(word.parents('.rotate-2').length > 0) letters[i] = '<em>' + letters[i] + '</em>';
-				letters[i] = (selected) ? '<i class="in">' + letters[i] + '</i>': '<i>' + letters[i] + '</i>';
-			}
-		    var newLetters = letters.join('');
-		    word.html(newLetters).css('opacity', 1);
-		});
-	}
+	$.fn.formreact=function(options){
 
-	function animateHeadline($headlines) {
-		var duration = animationDelay;
-		$headlines.each(function(){
-			var headline = $(this);
-			
-			if(headline.hasClass('loading-bar')) {
-				duration = barAnimationDelay;
-				setTimeout(function(){ headline.find('.cd-words-wrapper').addClass('is-loading') }, barWaiting);
-			} else if (headline.hasClass('clip')){
-				var spanWrapper = headline.find('.cd-words-wrapper'),
-					newWidth = spanWrapper.width() + 10
-				spanWrapper.css('width', newWidth);
-			} else if (!headline.hasClass('type') ) {
-				//assign to .cd-words-wrapper the width of its longest word
-				var words = headline.find('.cd-words-wrapper b'),
-					width = 0;
-				words.each(function(){
-					var wordWidth = $(this).width();
-				    if (wordWidth > width) width = wordWidth;
-				});
-				headline.find('.cd-words-wrapper').css('width', width);
-			};
+		var defauts={
+			selector:'',
+			wrapResult:'',
+			resultMsg:function(){},
+			wrapEnd:'',
+			endMsg:'<p>End message</p>',
+			'result':function(formValues){
+					// handle the returned values
+					$(this.wrapResult).html(this.resultMsg(formValues));
+					$(this.wrapEnd).html(this.endMsg);
+				}
+			},
+			param=$.extend(defauts, options),
+			classActive='react-active',
+			classCompleted='react-completed',
+			classCursor='typed-cursor',
+			elem=$(this),
+			i=0;
 
-			//trigger animation
-			setTimeout(function(){ hideWord( headline.find('.is-visible').eq(0) ) }, duration);
-		});
-	}
 
-	function hideWord($word) {
-		var nextWord = takeNext($word);
-		
-		if($word.parents('.cd-headline').hasClass('type')) {
-			var parentSpan = $word.parent('.cd-words-wrapper');
-			parentSpan.addClass('selected').removeClass('waiting');	
-			setTimeout(function(){ 
-				parentSpan.removeClass('selected'); 
-				$word.removeClass('is-visible').addClass('is-hidden').children('i').removeClass('in').addClass('out');
-			}, selectionDuration);
-			setTimeout(function(){ showWord(nextWord, typeLettersDelay) }, typeAnimationDelay);
-		
-		} else if($word.parents('.cd-headline').hasClass('letters')) {
-			var bool = ($word.children('i').length >= nextWord.children('i').length) ? true : false;
-			hideLetter($word.find('i').eq(0), $word, bool, lettersDelay);
-			showLetter(nextWord.find('i').eq(0), nextWord, bool, lettersDelay);
+		elem
 
-		}  else if($word.parents('.cd-headline').hasClass('clip')) {
-			$word.parents('.cd-words-wrapper').animate({ width : '2px' }, revealDuration, function(){
-				switchWord($word, nextWord);
-				showWord(nextWord);
-			});
+			.each(function(){
+				$(this)
+					.children('p')
+						.addClass(param.selector.replace('.',''));
+			})
 
-		} else if ($word.parents('.cd-headline').hasClass('loading-bar')){
-			$word.parents('.cd-words-wrapper').removeClass('is-loading');
-			switchWord($word, nextWord);
-			setTimeout(function(){ hideWord(nextWord) }, barAnimationDelay);
-			setTimeout(function(){ $word.parents('.cd-words-wrapper').addClass('is-loading') }, barWaiting);
+			// Put focus on the react-active input when hovering the wrapping div
+			.on('mouseover click',function(){
+				$(param.selector + '.' + classActive).find('input').focus();
+			})
 
-		} else {
-			switchWord($word, nextWord);
-			setTimeout(function(){ hideWord(nextWord) }, animationDelay);
-		}
-	}
+			.find(param.selector)
 
-	function showWord($word, $duration) {
-		if($word.parents('.cd-headline').hasClass('type')) {
-			showLetter($word.find('i').eq(0), $word, false, $duration);
-			$word.addClass('is-visible').removeClass('is-hidden');
+				.each(function(){
 
-		}  else if($word.parents('.cd-headline').hasClass('clip')) {
-			$word.parents('.cd-words-wrapper').animate({ 'width' : $word.width() + 10 }, revealDuration, function(){ 
-				setTimeout(function(){ hideWord($word) }, revealAnimationDelay); 
-			});
-		}
-	}
+					i++;
+					var $this=$(this);
 
-	function hideLetter($letter, $word, $bool, $duration) {
-		$letter.removeClass('in').addClass('out');
-		
-		if(!$letter.is(':last-child')) {
-		 	setTimeout(function(){ hideLetter($letter.next(), $word, $bool, $duration); }, $duration);  
-		} else if($bool) { 
-		 	setTimeout(function(){ hideWord(takeNext($word)) }, animationDelay);
-		}
+					$this
+						// reset the input values on browser refresh if necessary & append the necessary elements
+						.append('<input type="text" class="react-input" /><span class="content" ></span><span class="typed-cursor">&#9646;</span>')
+						.find('input')
+							.val('') ;
 
-		if($letter.is(':last-child') && $('html').hasClass('no-csstransitions')) {
-			var nextWord = takeNext($word);
-			switchWord($word, nextWord);
-		} 
-	}
+					if (i===1) {
+						//add an active class to the first selector and focus on the input
+						$this
+							.addClass(classActive)
+							.find('input')
+								.focus();
+						}
+				})
 
-	function showLetter($letter, $word, $bool, $duration) {
-		$letter.addClass('in').removeClass('out');
-		
-		if(!$letter.is(':last-child')) { 
-			setTimeout(function(){ showLetter($letter.next(), $word, $bool, $duration); }, $duration); 
-		} else { 
-			if($word.parents('.cd-headline').hasClass('type')) { setTimeout(function(){ $word.parents('.cd-words-wrapper').addClass('waiting'); }, 200);}
-			if(!$bool) { setTimeout(function(){ hideWord($word) }, animationDelay) }
-		}
-	}
 
-	function takeNext($word) {
-		return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
-	}
 
-	function takePrev($word) {
-		return (!$word.is(':first-child')) ? $word.prev() : $word.parent().children().last();
-	}
+				// Actions when the enter key is pressed
+				.children('input').on("keyup",function(e){
 
-	function switchWord($oldWord, $newWord) {
-		$oldWord.removeClass('is-visible').addClass('is-hidden');
-		$newWord.removeClass('is-hidden').addClass('is-visible');
-	}
-});
+					var $this=$(this);
+					if(e.keyCode === 13){
+		 	   			if ($this.val()!==''){
 
-$(function() {
-	$("#submitForm").click(handleBtnEvent);
-});
-function handleBtnEvent() {
-	var name = $("#name").val();
-	var email = $("#email").val();
-	var subject = $("#subject").val();
-	var message = $("#message").val();
+		 	   				$this
+		 	   					// prevents from going backwards and reediting a previous input
+		 	   					.prop('readonly', true)
+		 	   					.closest(param.selector)
+			 	   					.addClass(classCompleted)
+					                .removeClass(classActive);
+					    	$('.' + classCompleted + ' .' + classCursor).hide();
+					        $('.' + classActive + ' .' + classCursor).show();
 
-	if(name == "hello")
-	{
-		$("#showError").html("must be filled");
-		$("#showError").css('color','red');
-		return false;
-	} 
-	if(email == "")
-	{
-		$("#email").addClass("error");
-	}
-	if(subject == "")
-	{
-		$("#subject").addClass("error");
-	} 
-	if(message == "")
-	{
-		$("#message").addClass("error");
-	}
-}
+		 	   				// if another element exist, go to next input
+		 	   				if ($this.closest(param.selector).next().hasClass(param.selector.replace('.',''))){
+			 	   				$this
+			 	   					.closest(param.selector)
+						                .next(param.selector)
+						                    .addClass(classActive)
+						                    .find('input')
+						                        .focus();
+					    	}
+
+					    	// else if there is no more element
+				        	else {
+				        		var arrayReact=[];
+				        		$(param.selector).each(function(){
+				        			arrayReact.push($(this).find('input').val());
+				        		});
+				        		param.result(arrayReact);
+				        	}
+		 	   			}
+					}
+				})
+
+				// On input change, change the text displayed in the next span.content
+				.on("propertychange change click keyup input paste", function(){
+			        var el=$(this),
+			        	//finds the span where the text will be displayed
+			            display=$(this).closest(param.selector).find('.content');
+
+			        if (el.data('oldVal') != el.val()) {
+			            el.data('oldVal', el.val());
+			            display.html(el.val());
+			        }
+			        if (el.val()===''){
+			        	// Fix for chrome losing focus on deleting content
+			        	el.val('');
+			        }
+			    });
+
+		//CSS
+		$(param.selector).addClass('react-item');
+
+		return this;
+	};
+})(jQuery);
